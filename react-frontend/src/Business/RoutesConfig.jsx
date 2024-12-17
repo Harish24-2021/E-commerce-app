4f3813635b868ac148bf05ee52d98b217f9e1944
harish <harish.mph2014@gmail.com>
2024-12-18 02:41:05 +0530

    added react router and replaced exising static routing

---
+++ react-frontend/src/Business/RoutesConfig.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Route, Routes } from 'react-router'
+import Billing from '../Billing'
+import Cart from '../Cart'
+import Product from '../Product'
+import AddProducts from './Components/AddProducts'
+import Login from './Pages/Login'
+import Register from './Pages/Register'
+
+const RoutesConfig = () => {
+  return (
+    <div>
+ <Routes>
+    <Route path="/login" element={<Login />} />
+    <Route path="/register" element={<Register />} />
+    {/* <Route path="/logout" element={<Logout />} /> */}
+    <Route path="products" element={<Product />} />
+    <Route path="cart" element={<Cart />} />
+    {/* <Route path="checkout" element={<Checkout />} /> */}
+    <Route path="billing" element={<Billing />} />
+    <Route path="addproducts" element={<AddProducts />} />
+
+  </Routes>
+
+    </div>
+  )
+}
+
+export default RoutesConfig
