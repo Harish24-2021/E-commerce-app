13877e3a5fd0f5195a0bbc28d110fe7ec2b233f6
harish <harish.mph2014@gmail.com>
2024-12-22 22:26:40 +0530

    updated files

---
+++ react-frontend/src/util/constants.jsx
@@ -0,0 +1,7 @@
+export const home = "home";
+export const login= "login";
+export const logout = "logout";
+export const cart = "cart";
+export const products = "products";
+export  const checkout = "checkout";
+export const register = "register";
